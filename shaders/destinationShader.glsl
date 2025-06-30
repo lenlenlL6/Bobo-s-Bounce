4f95319bb5a4e8e32a6ce2425f541f0f6fbd741f
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2025-06-30 17:29:47 +0700

    remove texColor

--- shaders/destinationShader.glsl
+++ shaders/destinationShader.glsl
@@ -6,7 +6,6 @@ vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
         return color;
     }
 
-    vec4 texColor = Texel(tex, texture_coords);
     float dist = position.y - screen_coords.y;
     float fade = clamp(1 - dist/maxDistance, 0, 1);
     return vec4(color.rgb, color.a*fade);
